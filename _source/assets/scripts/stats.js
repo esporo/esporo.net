7ac8519c9c1177842e05c0786661aee659a94a29
arthr <email@arthr.me>
2018-12-25 03:54:06 -0200

    Trying to set credentials access to Spore REST API.

--- _source/assets/scripts/stats.js
+++ _source/assets/scripts/stats.js
@@ -2,6 +2,9 @@ $.ajax({
   url: "http://www.spore.com/rest/stats",
   cache: false,
   dataType: "xml",
+  xhrFields: {
+    withCredentials: true
+  },
   success: function(xml, status){
     totalUploads = xml.find('totalUploads').text(),
     totalUsers = xml.find('totalUsers').text(),
